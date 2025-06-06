cb853be58654e6d6210b0fc63473e951f2e83410
khorapedia <15696061+khorapedia@user.noreply.gitee.com>
2025-06-06 15:08:19 +0800

    feat: Update content from source SHA ff4d60d
    Source: https://github.com/Schluckauf-weg/DeutscheGrammatik.github.io.git

---
+++ docusaurus.config.js
@@ -0,0 +1,98 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '简明德语语法',
+  tagline: 'Deutsche Grammatik',
+  favicon: 'img/de-c.ico',
+
+  // Set the production url of your site here
+  url: 'https://schluckauf-weg.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/DeutscheGrammatik.github.io/',
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Schluckauf-weg', // Usually your GitHub org/user name.
+  projectName: 'DeutscheGrammatik.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: 'zh-Hans',
+      locales: ['zh-Hans', 'ja'],
+      localeConfigs: {
+        'zh-Hans': {
+          label: '简体中文',
+        },
+        ja: {
+          label: '日本語',
+        },
+      },
+    },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/Schluckauf-weg/DeutscheGrammatik.github.io/blob/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  plugins: [
+		[
+			require.resolve('docusaurus-lunr-search'), 
+			{
+				languages: ['zh','de'],
+			},
+		],
+	],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      // image: 'img/',
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'DG Logo',
+          src: 'img/de-s.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '简明德语语法',
+          },
+          {
+            href: 'https://github.com/Schluckauf-weg/DeutscheGrammatik.github.io.git',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+    }),
+};
+
+module.exports = config;
